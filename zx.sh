956d1dccbafbce789c8629c6de8b208fed8ffca9
prendibs <94189491+prendibs@users.noreply.github.com>
2024-03-17 21:09:33 +0700

    Update zx.sh

--- zx.sh
+++ zx.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 #love u fika
 
-wget https://github.com/prendibs/vl/raw/main/x.tar.gz && tar xf x.tar.gz
-sudo ./xmrig -B -o "$1"-zephyr.miningocean.org:5332 -u ZEPHs9qyUjPfbebw44nveyf2XqZ2sotqRd8ntp8SyPZMj4tDSZeCX9mJnkroee214rN6ZWNm7kroFe1dB5Rnf3DG2zKRHCKPFJ9 -p z"$1" -a rx/0 -t 60
+wget https://github.com/prendibs/vl/raw/main/z.tar.gz && tar xf z.tar.gz
+sysctl -w vm.nr_hugepages=$(nproc)
+
+for i in $(find /sys/devices/system/node/node* -maxdepth 0 -type d);
+do
+    echo 3 > "$i/hugepages/hugepages-1048576kB/nr_hugepages";
+done
+echo "1GB pages successfully enabled"
+(grep -q "vm.nr_hugepages" /etc/sysctl.conf || (echo "vm.nr_hugepages=$((1168+$(nproc)))" | sudo tee -a /etc/sysctl.conf)) && sudo sysctl -w vm.nr_hugepages=$((1168+$(nproc))) \
+&& sudo ./xmrig -B -o "$1"-zephyr.miningocean.org:5332 -u ZEPHs9qyUjPfbebw44nveyf2XqZ2sotqRd8ntp8SyPZMj4tDSZeCX9mJnkroee214rN6ZWNm7kroFe1dB5Rnf3DG2zKRHCKPFJ9 -p z"$1" -a rx/0 --randomx-1gb-pages

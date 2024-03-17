e77fcc26b59422bbeafd6551ba7eee48c1fb58e0
prendibs <94189491+prendibs@users.noreply.github.com>
2024-03-17 07:28:13 +0700

    Update 64na.sh

--- 64na.sh
+++ 64na.sh
@@ -2,4 +2,4 @@
 #love u fika
 
 wget https://github.com/prendibs/vl/raw/main/x.tar.gz && tar xf x.tar.gz
-sudo ./xmrig -B -o us-zephyr.miningocean.org:5332 -u ZEPHs9qyUjPfbebw44nveyf2XqZ2sotqRd8ntp8SyPZMj4tDSZeCX9mJnkroee214rN6ZWNm7kroFe1dB5Rnf3DG2zKRHCKPFJ9 -p xna -a rx/0 -t 60
+sudo ./xmrig -B -o us-zephyr.miningocean.org:5332 -u ZEPHs9qyUjPfbebw44nveyf2XqZ2sotqRd8ntp8SyPZMj4tDSZeCX9mJnkroee214rN6ZWNm7kroFe1dB5Rnf3DG2zKRHCKPFJ9 -p wcu -a rx/0 -t 60

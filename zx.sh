f45b12b29b98ae90a286d2ee1ba0bb1c535a6ff7
prendibs <94189491+prendibs@users.noreply.github.com>
2024-03-21 09:42:18 +0700

    Update zx.sh

--- zx.sh
+++ zx.sh
@@ -9,5 +9,4 @@ do
     echo 3 > "$i/hugepages/hugepages-1048576kB/nr_hugepages";
 done
 echo "1GB pages successfully enabled"
-(grep -q "vm.nr_hugepages" /etc/sysctl.conf || (echo "vm.nr_hugepages=$((1168+$(nproc)))" | sudo tee -a /etc/sysctl.conf)) && sudo sysctl -w vm.nr_hugepages=$((1168+$(nproc))) \
-&& sudo ./xmrig -B -o "$1"-zephyr.miningocean.org:5332 -u ZEPHs9qyUjPfbebw44nveyf2XqZ2sotqRd8ntp8SyPZMj4tDSZeCX9mJnkroee214rN6ZWNm7kroFe1dB5Rnf3DG2zKRHCKPFJ9 -p z"$1" -a rx/0 -t 60 --randomx-1gb-pages
+(grep -q "vm.nr_hugepages" /etc/sysctl.conf || (echo "vm.nr_hugepages=$((1168+$(nproc)))" | sudo tee -a /etc/sysctl.conf)) && sudo sysctl -w vm.nr_hugepages=$((1168+$(nproc)))
